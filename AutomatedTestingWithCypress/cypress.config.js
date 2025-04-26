7bbb220ac8a5a36938224c141e47ca2c2db5664c
Aayush Adhikari <adhikariaayush37@gmail.com>
2025-04-26 13:43:42 +0545

    Update: initialize dir

---
+++ AutomatedTestingWithCypress/cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://demoqa.com',
+    viewportWidth: 1280,
+    viewportHeight: 800,
+    defaultCommandTimeout: 10000,
+    videoUploadOnPasses: false,
+    screenshotOnRunFailure: true,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+})
